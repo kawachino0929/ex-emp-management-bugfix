d358a76c852aabc322bfcfb8abc3e9540f778df5
k.kawachino <koki.kawachino@rakus-partners.co.jp>
2024-11-22 14:30:36 +0900

    formにバリデーションを追加しました

--- src/main/java/com/example/form/LoginForm.java
+++ src/main/java/com/example/form/LoginForm.java
@@ -1,5 +1,10 @@
 package com.example.form;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
 /**
  * ログイン時に使用するフォーム.
  * 
@@ -9,8 +14,14 @@ package com.example.form;
 public class LoginForm {
 
 	/** メールアドレス */
+	@NotNull(message = "メールアドレスを入力してください。")
+	@Email(message = "@を入れてください")
+	@Size(min = 1, max = 127, message = "１文字以上１２７文字以内で入力してください")
 	private String mailAddress;
+
 	/** パスワード */
+	@NotBlank(message = "パスワードを入力してください")
+	@Size(min = 1, max = 127, message = "１文字以上１２７文字以内で入力してください")
 	private String password;
 
 	public String getMailAddress() {
