db02341cb061c98c17923c81eb8f05e2756c7ab1
k.kawachino <koki.kawachino@rakus-partners.co.jp>
2024-11-22 14:29:23 +0900

    htmlにバリデーションを追加しました

--- src/main/java/com/example/form/InsertAdministratorForm.java
+++ src/main/java/com/example/form/InsertAdministratorForm.java
@@ -2,6 +2,7 @@ package com.example.form;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 /**
@@ -17,10 +18,14 @@ public class InsertAdministratorForm {
 	private String name;
 
 	/** メールアドレス */
+	@NotNull(message = "メールアドレスを入力してください。")
 	@Email(message = "@を入れてください")
 	@Size(min = 1, max = 127, message = "１文字以上１２７文字以内で入力してください")
 	private String mailAddress;
+
 	/** パスワード */
+	@NotBlank(message = "パスワードを入力してください")
+	@Size(min = 1, max = 127, message = "１文字以上１２７文字以内で入力してください")
 	private String password;
 
 	public String getName() {
