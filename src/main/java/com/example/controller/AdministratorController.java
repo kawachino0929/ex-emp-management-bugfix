3af144187179fa3ead4323d19a2c62b9f7e10887
k.kawachino <koki.kawachino@rakus-partners.co.jp>
2024-11-25 16:26:49 +0900

    修正いたしました

--- src/main/java/com/example/controller/AdministratorController.java
+++ src/main/java/com/example/controller/AdministratorController.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -75,19 +76,21 @@ public class AdministratorController {
 	 * @return ログイン画面へリダイレクト
 	 */
 	@PostMapping("/insert")
-	public String insert(@Validated InsertAdministratorForm form, BindingResult result, Model model) {
+	public String insert(@Validated(InsertAdministratorForm.All.class) InsertAdministratorForm form, BindingResult result, Model model) {
 		if (result.hasErrors()) {
 			return toInsert();
 		}
 		Administrator administrator = new Administrator();
 		// フォームからドメインにプロパティ値をコピー
 		BeanUtils.copyProperties(form, administrator);
-		try {
+		if (administratorService.findByMail(form.getMailAddress()) == null) {
 			administratorService.insert(administrator);
-		} catch (Exception e) {
-			model.addAttribute("errorMessage", "そのメールアドレスは既に登録されています");
+		} else {
+			FieldError fieldError = new FieldError(result.getObjectName(), "mailAddress", form.getMailAddress(), false, null, null, "そのメールアドレスは既に使われています");
+			result.addError(fieldError);
 			return toInsert();
 		}
+
 		return "redirect:/";
 	}
 
@@ -111,7 +114,7 @@ public class AdministratorController {
 	 * @return ログイン後の従業員一覧画面
 	 */
 	@PostMapping("/login")
-	public String login(@Validated LoginForm form, BindingResult result, RedirectAttributes redirectAttributes) {
+	public String login(@Validated(LoginForm.All.class) LoginForm form, BindingResult result, RedirectAttributes redirectAttributes) {
 		if (result.hasErrors()) {
 			return toLogin();
 		}
